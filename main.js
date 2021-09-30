bfecbbcd62f5d49c229596517f91024995d0e3e7
WaterBlaster985 <74526954+WaterBlaster985@users.noreply.github.com>
2021-09-30 15:54:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preLoad(){
+
+}
+function setup(){
+    canvas= createCanvas(300, 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    video.size(300, 300);
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("modelLoaded")
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log("results");
+        console.log("noseX= "+results[0].pose.nose.x);
+        console.log("noseY= "+results[0].pose.nose.y);
+    }
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+function take_snapshot(){
+    save(myfilterimage.png);
+}
